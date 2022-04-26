86818b4ee07d03c02257a1a6311266466f3edad0
anyonejustnotmyself <dariapuzyreva@stud.tsu.ru>
2022-04-26 22:03:04 +0700

    Добавлена кнопка открытия меню блоков (пока пустое)

---
+++ app/src/main/java/com/example/mobile_module/BlockMenu.kt
@@ -0,0 +1,11 @@
+package com.example.mobile_module
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BlockMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_block_menu)
+    }
+}
--- app/src/main/java/com/example/mobile_module/MainActivity.kt
+++ app/src/main/java/com/example/mobile_module/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.mobile_module
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.newBlockBtn).setOnClickListener {
+            val openBlockMenu = Intent(this, BlockMenu::class.java)
+            startActivity(openBlockMenu)
+        }
     }
 
     val vars:Vars = Vars()
@@ -22,4 +29,5 @@ class MainActivity : AppCompatActivity() {
         editNameVar.setText("")
         editAmountVar.setText("")
     }
+
 }
